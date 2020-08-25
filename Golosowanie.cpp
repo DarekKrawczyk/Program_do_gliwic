8624df6693b6564677c19c9c6372a11866e91436
Darek Krawczyk <Darek9Krawczyk@hotmail.com>
2020-08-25 18:30:10 +0200

    Dodaj pliki projektów.

---
+++ Golosowanie.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <string>
+#include <filesystem>
+
+int main()
+{
+    std::string path = "/path/to/directory";
+    for (const auto& entry : std::filesystem::directory_iterator(path))
+        std::cout << entry.path() << std::endl;
+}
